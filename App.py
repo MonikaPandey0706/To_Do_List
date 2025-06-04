d94e83a79b74cd5ab9c1edcd13ee8288f38de6be
Kavya M <kavya.m@s.amity.edu>
2025-06-04 12:37:42 +0530

    Imported libraries and initialized constants

--- App.py
+++ App.py
@@ -1,1 +1,15 @@
 #Intialized a Python File
+
+import json
+import os
+from datetime import datetime, timedelta
+
+# --- Constants ---
+# File to store task data.
+TASKS_FILE = 'student_tasks.json'
+
+# Allowed task priorities.
+ALLOWED_PRIORITIES = ["High", "Medium", "Low"]
+
+# Allowed task statuses.
+ALLOWED_STATUSES = ["Pending", "Completed"]
